7eb2c18860e22d756849bd8461cea788dcd45c1e
RaimuHosoda <adwyaatd0601@gmail.com>
2020-08-02 17:40:30 +0900

    リンク先を変更
    リンク先を変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -15,10 +15,14 @@ class HomesController < ApplicationController
 
 
 	def scr
+		#headlessモード
 		options = Selenium::WebDriver::Chrome::Options.new
 		options.add_argument('--headless')
-
 		d = Selenium::WebDriver.for :chrome, options: options
+
+		#通常モード
+		# d = Selenium::WebDriver.for :chrome
+
 		d.navigate.to 'https://kakaku.com/game/game-console/itemlist.aspx?pdf_se=16'
 
 		@content = d.find_element(:xpath,'//*[@id="main"]/div[2]/div[1]/div/table/tbody/tr[2]/td[1]')
