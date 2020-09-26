4c08f64f74c4015f9d591ea9e5f270bb0e487e67
RaimuHosoda <adwyaatd0601@gmail.com>
2020-09-26 18:14:12 +0900

    ログ出力、毎日ログ初期化、エラー非表示化

--- app/models/s2dmm.rb
+++ app/models/s2dmm.rb
@@ -1,111 +1,81 @@
 class S2dmm < ApplicationRecord
 	def self.scr2
-		# require 'open_uri'
-		# headlessモード
-		options = Selenium::WebDriver::Chrome::Options.new
-		options.add_argument('--headless')
-		d = Selenium::WebDriver.for :chrome, options: options
 
-		# #通常モード
-		# d = Selenium::WebDriver.for :chrome
-
-		# 検索ページへ遷移
-		d.navigate.to 'https://www.google.co.jp/imghp?hl=ja&tab=ri&authuser=0&ogbl'
-
-		sleep(2)
-
-		#入力した内容を検索バーに入力
-
-		# search_bar = d.find_element(:xapth,"id('sbtc')//input[contains(@title,'検索')]")
-		search_bar = d.find_element(:name,"q")
-		# search_bar.send_key(params[:word], :enter)
-		search_bar.send_key("レモン", :enter)
-		sleep(2)
-
-		#検索
-		# search_bar.submit
-		# sleep(2)
-
-		#ライセンスフリーの画像に絞り込む（「ツール」→「ライセンス」→「再使用が許可された画像」を選択）
-		#ツールをクリック
-		tool = d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div[2]/div/div")
-		tool.click
-		sleep(1)
-		#ライセンスをクリック
-		begin
-			pp "①"
-			pull_down = d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[2]/c-wiz[1]/div/div/div[1]/div/div[3]/div/div[1]")
-		rescue
-			pp "①NG"
-		end
-
-		# begin
-		# 	pp "②"
-		# 	pull_down = d.find_element(:xpath,"id('yDmH0d')//div[contains(@aria-label,'ライセンス')")
-		# rescue
-		# 	pp "②NG"
-		# end
-		pull_down.click
-		sleep(2)
-
-		#プルダウンから再使用が許可された画像を選択
-		l = d.find_element(:link_text,"再使用が許可された画像")
-		l.click
-		sleep(2)
-
-		#画像要素を取得
-		# imgs = d.find_elements(:xpath,"id('islrg')/div[1]/div/a[1]/div[1]/img")
-		pp __LINE__
-		begin
-			pp "①"
-			imgs = d.find_elements(:xpath,"id('islrg')//img")
-		rescue
-			pp "①NG"
-		end
-		pp "ok"
-		sleep(5)
-
-		n=1
 		array = []
-		imgs.each do |i|
-			img_src = i.attribute('src')
-			array[n-1] = img_src
 
-			n +=1
-			if n == 11
-				pp "OKOK"
-				break
+		wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+		retry_cnt = 0
+
+		begin
+			begin
+				# require 'open_uri'
+				# pp "headlessモード"
+				# options = Selenium::WebDriver::Chrome::Options.new
+				# options.add_argument('--headless')
+				# d = Selenium::WebDriver.for :chrome, options: options
+
+				pp "通常モード"
+				d = Selenium::WebDriver.for :chrome
+
+				pp "検索ページへ遷移"
+				d.navigate.to 'https://www.google.co.jp/imghp?hl=ja&tab=ri&authuser=0&ogbl'
+			rescue => e
+				logger.error("----------------------[error]file:#{$0},line:#{__LINE__},error:#{e}---------------------------")
+				sleep(1)
+				retry_cnt += 1
+				pp retry_cnt
+				retry if retry_cnt <= 5
 			end
+
+			pp"入力した内容を検索バーに入力して検索"
+			search_bar = wait.until{ d.find_element(:name,"q") }
+			search_bar.send_key("レモン", :enter)
+
+			pp "ライセンスフリーの画像に絞り込む（「ツール」→「ライセンス」→「再使用が許可された画像」を選択）"
+			pp "ツールをクリック"
+			tool = wait.until{ d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div[2]/div/div") }
+			tool.click
+
+			sleep(2)
+
+			pp "ライセンスをクリック"
+			pull_down = wait.until{ d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[2]/c-wiz[1]/div/div/div[1]/div/div[5]/div/div[1]") }
+
+			begin
+				pull_down.click
+			rescue
+				pp __LINE__
+				d.navigate.refresh
+				tool = wait.until{ d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div[2]/div/div") }
+				tool.click
+				sleep(3)
+				pull_down = wait.until{ d.find_element(:xpath,"//*[@id=\"yDmH0d\"]/div[2]/c-wiz/div[2]/c-wiz[1]/div/div/div[1]/div/div[5]/div/div[1]") }
+				pull_down.click
+			end
+
+			pp "プルダウンから「クリエイティブ・コモンズ ライセンス」を選択"
+			l = wait.until{ d.find_element(:link_text,"クリエイティブ・コモンズ ライセンス") }
+			l.click
+
+			pp "画像要素を取得"
+			imgs = wait.until{ d.find_elements(:xpath,"id('islrg')//img") }
+
+			imgs.each_with_index do |i,n|
+				img_src = i.attribute('src')
+				array[n] = img_src
+			end
+
+			# array = array.delete_if(&:empty?)
+			array = array.compact
+
+			pp "AllOK"
+			d.quit
+			return array
+		rescue => e
+			pp "エラー発生"
+			logger.error("----------------------[error]file:#{$0},line:#{__LINE__},error:#{e}---------------------------")
+			d.quit
+			return array
 		end
-
-		# n=1
-		# imgs.each do |i|
-		# 	@imgsrc = i.attribute('src')
-		# 	pp imgsrc
-		# 	n +=1
-		# 	if n == 11
-		# 		break
-		# 	end
-		# end
-
-		#取得した要素から画像を繰り返し処理で1つずつデータベースに保管
-
-		# imgs.each do |img|
-		# 	img_url = img.attribute('href')
-		# 	File.open(params[:name]+n,"wb") do |file|
-		# 		open(img_url) do |i|
-		# 			file.puts i.read
-		# 		end
-		# 	end
-		# 	n += 1
-		# 	if n==31
-		# 		break
-		# 	end
-		# end
-
-		#終了？
-		pp "AllOK"
-		d.quit
-		return array
 	end
 end
