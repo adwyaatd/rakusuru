5ae3bbcf8d6b6528bd98d9ab992385eeee0d6193
RaimuHosoda <adwyaatd0601@gmail.com>
2021-04-01 18:45:00 +0900

    jQueryを導入　一括チェックを実装

---
+++ app/javascript/packs/s3base.js
@@ -0,0 +1,29 @@
+$(document).on('turbolinks:load', function(){
+	$('#check_all').on('click', function() {
+		$("input[type=checkbox],[id^=ids_]").prop('checked', this.checked);
+	});
+
+	$("input[type=checkbox],[id^=ids_]").on('click',function(){
+		var $count_checked = $("input[id^=ids_]").filter(":checked").length;
+		var $count_input = $("input[id^=ids_]").length;
+
+		if ($count_checked == $count_input){
+			$("#check_all").prop('checked', 'checked');
+		}else{
+			$('#check_all').prop('checked', false);
+		}
+	});
+});
+
+
+// $(document).on('turbolinks:load', function(){
+//   $('p').on('click', function() {
+//     $(this).css('color', 'blue');
+//   });
+// });
+
+
+$('p').on('click', function() {
+	$(this).css('color', 'blue');
+});
+
