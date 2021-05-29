4bac18965f80699125c4fe206038da6552646fe2
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-05-29 13:26:28 +0900

    aws 認証情報設定を追加

--- app/models/sqs.rb
+++ app/models/sqs.rb
@@ -1,6 +1,10 @@
 class Sqs < ApplicationRecord
   def self.send_message(queue_name,message_json,i=0)
-		sqs = Aws::SQS::Client.new(region: 'ap-northeast-1')
+		if Rails.env.development?
+			sqs = Aws::SQS::Client.new(region: 'ap-northeast-1',profile: "default")
+		elsif Rails.env.production?
+			sqs = Aws::SQS::Client.new(region: 'ap-northeast-1',profile: "RAIMU")
+		end
 		queue_url = sqs.get_queue_url(queue_name: queue_name).queue_url
 
 		send_message_result = sqs.send_message({
